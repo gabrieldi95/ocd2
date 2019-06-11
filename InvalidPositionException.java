e123d1c406e155b1596ae4d8f191d9cbe0bfdfaa
Gabriel Arruda <gabrielpardi@outlook.com>
2019-06-11 20:51:04 -0300

    mudança da exception e comentarios

---
+++ InvalidPositionException.java
@@ -0,0 +1,5 @@
+public class InvalidPositionException extends Exception {
+    public InvalidPositionException(String error){
+        super(error);
+    }
+}
--- Memoria.java
+++ Memoria.java
@@ -1,25 +1,31 @@
 import java.util.LinkedList;
 
+/*
+    * Lista ligada com métodos personalizados para a memória
+ */
 public class Memoria extends LinkedList<Bloco> {
 
     void insere(int valor, int pos){
 
+        // Busca primeiro se ejá existe um bloco nessa posição e coloca no lugar dele
         for(Bloco e : this){
             if(e.pos == pos) {
                 set(indexOf(e), new Bloco(valor, pos));
             }
         }
+        // Se não tiver insere no final da lista
         add(new Bloco(valor, pos));
 
     }
 
     int getMem(int pos){
-
         for (Bloco e : this)
             if(e.pos == pos) return e.valor;
+
+        // Se não achar um bloco com essa posição joga uma exceção
         try {
-            throw new InvalidIndexException("Posicao nao encontrada");
-        } catch (InvalidIndexException e) {
+            throw new InvalidPositionException("Posicao nao encontrada");
+        } catch (InvalidPositionException e) {
             e.printStackTrace();
         }
         return 0;
